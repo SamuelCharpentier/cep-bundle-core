8ba32ff09811e76c87cf136f4732f089f7790130
Samuel Charpentier <scharpentier0@gmail.com>
2021-06-24 18:07:01 -0400

    trying to debug removal of unused lines

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  semi: false,
-  trailingComma: 'all',
-  singleQuote: true,
-  printWidth: 120,
-  tabWidth: 2
-};
-
+	semi: true,
+	trailingComma: 'all',
+	singleQuote: true,
+	printWidth: 120,
+	tabWidth: 4,
+	useTabs: true,
+}
