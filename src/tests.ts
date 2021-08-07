678a2f4fba413572da2f7f051724e2b465f056fa
Samuel Charpentier <scharpentier0@gmail.com>
2021-08-06 22:34:51 -0400

    Context for tests

--- src/tests.ts
+++ src/tests.ts
@@ -1,10 +1,13 @@
-import { ExtensionList } from './lib/ExtensionList';
+import { ExtensionList, isExtensionListArgument } from './lib/ExtensionList';
 import { ExtensionManifest, ExtensionManifestArgument, isExtensionManifestArgument } from './lib/ExtensionManifest';
 import cepConfig from '../cepConfig.js';
 
 const config = cepConfig;
-
-if (isExtensionManifestArgument(config)) {
-	console.log('<?xml version="1.0" encoding="UTF-8"?>\n' + new ExtensionManifest(config).xml(['manifest.xml']));
-	console.log('<?xml version="1.0" encoding="UTF-8"?>\n' + new ExtensionList(config.extensions).xml(['.debug']));
+const manifestConfig = { ...config.manifest, extensions: config.extensions };
+if (isExtensionManifestArgument(manifestConfig)) {
+	console.log(
+		'<?xml version="1.0" encoding="UTF-8"?>\n' + new ExtensionManifest(manifestConfig).xml(['manifest.xml']),
+	);
 }
+if (isExtensionListArgument(config.extensions))
+	console.log('<?xml version="1.0" encoding="UTF-8"?>\n' + new ExtensionList(config.extensions).xml(['.debug']));
