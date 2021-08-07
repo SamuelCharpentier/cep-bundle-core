b2924d5a8e613b618c2f7e7dcc4fcde7e22633d8
Samuel Charpentier <scharpentier0@gmail.com>
2021-08-06 22:37:08 -0400

    Added extension list argument validator

--- src/lib/ExtensionList.ts
+++ src/lib/ExtensionList.ts
@@ -1,6 +1,28 @@
 import { XMLElement } from './XMLElement';
 import { Extension, ExtensionArgument, isExtensionArgument } from './Extension';
 import { badArgumentError } from './errorMessages';
+
+export type ExtensionListArgument = ExtensionArgument | ExtensionArgument[];
+
+export const isExtensionListArgument = <(arg: any) => arg is ExtensionListArgument>((extensions) => {
+	if (!extensions)
+		throw new Error(
+			badArgumentError('extensions', 'an object or array of objects of type ExtensionArgument', extensions),
+		);
+	if (!(extensions instanceof Array)) extensions = [extensions];
+	for (const extension of extensions) {
+		if (!isExtensionArgument(extension))
+			throw new Error(
+				badArgumentError(
+					'extensions',
+					'object or array of objects of type ExtensionArgument(type)\nThe following element was problematic',
+					extension,
+				),
+			);
+	}
+	return true;
+});
+
 export class ExtensionList extends XMLElement {
 	constructor(extensions: ExtensionArgument | ExtensionArgument[]) {
 		if (!(extensions instanceof Array)) extensions = [extensions];
