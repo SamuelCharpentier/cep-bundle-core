2be3700791f5c19f8a44614e5cc1414a92d019ea
Samuel Charpentier <scharpentier0@gmail.com>
2021-08-06 22:42:50 -0400

    Starting to work on the default configs

--- src/config/getDefaultConfig.ts
+++ src/config/getDefaultConfig.ts
@@ -1,29 +1,32 @@
 export function getDefaultConfig() {
 	return {
 		out: './dist',
-		hosts: '*',
+		root: process.cwd(),
 		env: process.env.NODE_ENV,
 		debugInProduction: false,
-		cepVersion: '8.0',
-		lifecycle: {
-			autoVisible: true,
-			startOnEvents: [],
+		extensionBundle: { cepVersion: '8.0' },
+		extension: {
+			hostList: 'All',
+			dispatchInfo: {
+				lifecycle: {
+					autoVisible: true,
+				},
+			},
 		},
-		cefParams: ['--allow-file-access-from-files', '--allow-file-access', '--enable-nodejs', '--mixed-context'],
-		debugPorts: {
-			PHXS: 3001,
-			IDSN: 3002,
-			AICY: 3003,
-			ILST: 3004,
-			PPRO: 3005,
-			PRLD: 3006,
-			AEFT: 3007,
-			FLPR: 3008,
-			AUDT: 3009,
-			DRWV: 3010,
-			MUST: 3011,
-			KBRG: 3012,
-		},
-		root: process.cwd(),
 	};
 }
+
+export const defaultDebugPorts = {
+	PHXS: 3001,
+	IDSN: 3002,
+	AICY: 3003,
+	ILST: 3004,
+	PPRO: 3005,
+	PRLD: 3006,
+	AEFT: 3007,
+	FLPR: 3008,
+	AUDT: 3009,
+	DRWV: 3010,
+	MUST: 3011,
+	KBRG: 3012,
+};
