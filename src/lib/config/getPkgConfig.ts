7c8b7cf80ef1284375ee1d96a004d72503200aca
Samuel Charpentier <scharpentier0@gmail.com>
2021-09-21 15:19:42 -0400

    Working on getting configs

--- src/lib/config/getPkgConfig.ts
+++ src/lib/config/getPkgConfig.ts
@@ -1,12 +1,16 @@
 import { existsSync } from 'fs-extra';
 import path from 'path';
 
-export function getPkgConfig(root: string): { [key: string]: any } {
-	let packageJSONPath = path.join(root, '/package.json');
-	if (existsSync(packageJSONPath)) {
-		const config = require(packageJSONPath).cep;
-		return config;
-	} else {
-		throw new Error(`Could not proceed without package.json at ${packageJSONPath}`);
+export function getPkgConfig(root?: string): { [key: string]: any } {
+	if (root) {
+		let packageJSONPath = path.join(root, '/package.json');
+		if (existsSync(packageJSONPath)) {
+			const config = require(packageJSONPath).cep;
+			if (config === undefined) return {};
+			return config;
+		} else {
+			throw new Error(`Could not proceed without package.json at ${packageJSONPath}`);
+		}
 	}
+	return {};
 }
