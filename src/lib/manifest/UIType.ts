16989e128a6802f272a650b5e67673cffa038162
Samuel Charpentier <scharpentier0@gmail.com>
2021-10-22 12:12:39 -0400

    Optimizing and testing Type class

--- src/lib/manifest/UIType.ts
+++ src/lib/manifest/UIType.ts
@@ -2,25 +2,18 @@ import { XMLElement } from './XMLElement';
 import { UIType, isUIType } from './enumsAndValidators';
 import { badArgumentError } from '../errorMessages';
 
-export type TypeArgument = UIType | keyof typeof UIType;
+export type TypeArgument = UIType | `${UIType}` | keyof typeof UIType;
 
 export const isTypeArgument: (value: any) => boolean = (value): value is TypeArgument => {
-	if (value) {
-		if (!isUIType(value))
-			throw new Error(badArgumentError('extension.dispatchInfo.ui.type', 'a UIType(enum)', value));
+	if (value !== undefined && isUIType(value)) {
 		return true;
 	}
-	return false;
+	throw new Error(badArgumentError('Type', 'a UIType (enum), `${UIType}` or keyof typeof UIType', value));
 };
 export class Type extends XMLElement {
 	constructor(type: TypeArgument) {
-		let content: string;
-		if (type && isUIType(type)) content = type;
-		else
-			throw new Error(
-				badArgumentError('Type only argument', 'string of a keyof typeof UIType(enum) or UIType(enum)', type),
-			);
-
-		super({ name: 'Type', content });
+		if (isTypeArgument(type)) {
+			super({ name: 'Type', content: type });
+		}
 	}
 }
