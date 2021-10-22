09887ebabf99cd7f2ec25562d15c67c09afacf8b
Samuel Charpentier <scharpentier0@gmail.com>
2021-10-22 12:12:22 -0400

    Optimizing and testing Menu class

--- src/lib/manifest/Menu.ts
+++ src/lib/manifest/Menu.ts
@@ -1,36 +1,31 @@
 import { XMLElement } from './XMLElement';
-import { AttributeArgument, Placement, isPlacement } from '../typesAndValidators';
+import { AttributeArgument } from './XMLElement';
+import { Placement, isPlacement } from '../typesAndValidators';
 import { badArgumentError, printVariableInError } from '../errorMessages';
 
-export type MenuArgument = { menuName: string; placement?: Placement };
+export interface MenuArgument {
+	menuName: string;
+	placement?: Placement;
+}
 
-export const isMenuArgument: (arg: any) => boolean = (arg) => {
-	if (arg) {
+export function isMenuArgument(arg: any): arg is MenuArgument {
+	if (arg !== undefined && typeof arg === 'object' && !(arg instanceof Array)) {
 		if (typeof arg.menuName !== 'string')
-			throw new Error(badArgumentError('extension.dispatchInfo.ui.menu.menuName', 'a string', arg.menuName));
-		if (arg.placement && !isPlacement(arg.placement)) throw new Error(badArgumentError('', '', arg.placement));
+			throw new Error(badArgumentError('menu.menuName', 'a string', arg.menuName));
+		if (arg.placement && !isPlacement(arg.placement))
+			throw new Error(badArgumentError('menu.placement (optional)', 'as a Placement (type)', arg.placement));
 		return true;
 	}
-	throw new Error('extension.dispatchInfo.ui.menu could not be validated' + printVariableInError(arg));
-
-	return false;
-};
+	throw new Error(badArgumentError('menu', 'as a MenuArgument (interface)', arg));
+}
 export class Menu extends XMLElement {
-	constructor({ menuName, placement }: MenuArgument) {
-		if (menuName && typeof menuName === 'string') {
+	constructor(menuArgument: MenuArgument) {
+		if (isMenuArgument(menuArgument)) {
+			const { menuName, placement } = menuArgument;
 			let attributes: AttributeArgument | undefined = undefined;
-			if (placement)
-				if (!isPlacement(placement))
-					throw new Error(
-						badArgumentError(
-							"Menu's placement(optional)",
-							'Placement(type) or string containing a valid Placement(type)',
-							placement,
-						),
-					);
-				else attributes = { name: 'Placement', value: placement };
+			if (placement !== undefined) attributes = { name: 'Placement', value: placement };
 
 			super({ name: 'Menu', attributes, content: menuName });
-		} else throw new Error(badArgumentError("Menu's name", 'string', menuName));
+		}
 	}
 }
