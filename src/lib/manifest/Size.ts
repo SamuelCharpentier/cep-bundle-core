fe47744e7b4f69757cc81a019caf7c2ef1a56bb9
Samuel Charpentier <scharpentier0@gmail.com>
2022-01-05 11:58:54 -0500

    Size is now managed within Geometry

--- src/lib/manifest/Size.ts
+++
@@ -1,4 +0,0 @@
-import { XMLElement } from './XMLElement';
-import { StringContent } from './StringContent';
-import { bothWidthAndHeightRequired, badArgumentError } from '../errorMessages';
-import { SizesTypes } from './enumsAndValidators';
