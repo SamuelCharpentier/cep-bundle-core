3cd77fcbe6c6d50ff7d7abdca24e15e72f51922b
Samuel Charpentier <scharpentier0@gmail.com>
2023-03-22 07:57:07 -0400

    Test getAllHostList

---
+++ __tests__/manifestConfigs/getAllHostList.test.ts
@@ -0,0 +1,19 @@
+import { HostEngine, getHostEngineKey } from '@src/lib/enumsAndValidators';
+import { getAllHostList } from '@src/manifestConfigs/getAllHostList';
+
+describe('getAllHostList', () => {
+	it('is defined', () => {
+		expect(getAllHostList).toBeDefined();
+	});
+	it('returns an array of all hosts', () => {
+		const hostList = getAllHostList();
+		expect(hostList).toBeInstanceOf(Array);
+		expect(hostList.length).toBe(Object.keys(HostEngine).length);
+		hostList.forEach((host) => {
+			expect(host).toHaveProperty('host');
+			expect(host).toHaveProperty('version');
+			expect(host).toHaveProperty('debugPort');
+			expect(HostEngine).toHaveProperty(getHostEngineKey(host.host));
+		});
+	});
+});
