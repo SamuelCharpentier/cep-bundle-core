2c4ce1fbc1e078b51532bf824cc3f72e27c9ea94
Samuel Charpentier <scharpentier0@gmail.com>
2023-03-22 08:41:16 -0400

    Test getExtensions

---
+++ __tests__/manifestConfigs/getExtensions.test.ts
@@ -0,0 +1,118 @@
+import { getExtensions } from '@src/manifestConfigs/getExtensions';
+import { _UserManifestConfigs } from '@src/userConfigs/UserManifestConfigs';
+import { ManifestConfigs } from '@src/manifestConfigs/convertToManifestConfigs';
+import { version } from 'yargs';
+import { HostEngine } from '@src/lib/enumsAndValidators';
+import { _Extension } from '@src/userConfigs/Extension';
+import { AllDispatchInfo } from '@src/userConfigs/AllDispatchInfo';
+import {
+	VisibleDispatchInfo,
+	isVisibleDispatchInfo,
+} from '@src/userConfigs/VisibleDispatchInfo';
+
+type Descriptor = string;
+type ArgumentValue = _UserManifestConfigs['extensions'];
+type ExpectedValue = ManifestConfigs['extensions'];
+
+const dispatchInfo = {
+	resources: {
+		htmlPath: './index.html',
+		scriptPath: './index.js',
+	},
+	ui: {
+		type: 'Panel',
+		geometry: {
+			screenPercentage: {
+				width: 0.5,
+				height: 0.5,
+			},
+		},
+	},
+} as AllDispatchInfo;
+
+const cases: [Descriptor, ArgumentValue, ExpectedValue][] = [
+	[
+		'a single extension',
+		{
+			id: 'test',
+			version: '1.0.0',
+			hostList: {
+				host: 'Photoshop',
+			},
+			dispatchInfo,
+		},
+		[
+			{
+				id: 'test',
+				version: '1.0.0',
+				hostList: [
+					{
+						host: HostEngine.Photoshop,
+						version: '[0,999]',
+						debugPort: 3001,
+					},
+				],
+				dispatchInfo: [dispatchInfo as VisibleDispatchInfo],
+			},
+		],
+	],
+	[
+		'multiple extensions',
+		[
+			{
+				id: 'test',
+				version: '1.0.0',
+				hostList: {
+					host: 'Photoshop',
+				},
+				dispatchInfo,
+			},
+			{
+				id: 'test2',
+				version: '1.0.0',
+				hostList: {
+					host: 'InDesign',
+				},
+				dispatchInfo,
+			},
+		],
+		[
+			{
+				id: 'test',
+				version: '1.0.0',
+				hostList: [
+					{
+						host: HostEngine.Photoshop,
+						version: '[0,999]',
+						debugPort: 3001,
+					},
+				],
+				dispatchInfo: [dispatchInfo as VisibleDispatchInfo],
+			},
+			{
+				id: 'test2',
+				version: '1.0.0',
+				hostList: [
+					{
+						host: HostEngine.InDesign,
+						version: '[0,999]',
+						debugPort: 3002,
+					},
+				],
+				dispatchInfo: [dispatchInfo as VisibleDispatchInfo],
+			},
+		],
+	],
+];
+
+describe('getExtensions', () => {
+	it('is defined', () => {
+		expect(getExtensions).toBeDefined();
+	});
+	test.each(cases)(
+		'getExtensions returns %s given %s',
+		(descriptor, arg, expected) => {
+			expect(getExtensions(arg)).toEqual(expected);
+		},
+	);
+});
